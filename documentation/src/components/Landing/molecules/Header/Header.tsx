16fd30129d2fe74b7015efcc3ce71a076062f91c
Dmitrii Putilin <dlputilin@dobry-kot.ru>
2025-03-26 17:12:48 +0300

    fix

--- documentation/src/components/Landing/molecules/Header/Header.tsx
+++ documentation/src/components/Landing/molecules/Header/Header.tsx
@@ -1,18 +1,26 @@
 import React, { FC } from 'react'
 import Link from '@docusaurus/Link'
+import BrowserOnly from '@docusaurus/BrowserOnly'; // Импортируем компонент BrowserOnly
 import styles from './styles.module.css'
-const getBasePrefix = () => window.BASE_PREFIX || ''
 
+// Компонент Header
 export const Header: FC = () => (
   <div className={styles.container}>
     <div className={styles.text}>
       <h1 className="header-title">
-        <Link to="/tech-docs/kubernetes/about">
-          <img src={`${getBasePrefix()}/img/paws.png`} alt="Logo" className="logo2" />
-        </Link>
+        <BrowserOnly>
+          {() => {
+            const getBasePrefix = () => window.BASE_PREFIX || '';
+            return (
+              <Link to={`${getBasePrefix()}/tech-docs/kubernetes/about`}>
+                <img src={`${getBasePrefix()}/img/paws.png`} alt="Logo" className="logo2" />
+              </Link>
+            );
+          }}
+        </BrowserOnly>
         B-Cloud
       </h1>
-      <p className="subheading">Строим вместе</p>
+      <p className="subheading">Строим вместе облако</p>
     </div>
   </div>
 )
