b7b861d9a469e2eace11e240d724c1e70bb06902
Dmitrii Putilin <dlputilin@dobry-kot.ru>
2025-03-26 16:37:19 +0300

    fix

--- documentation/src/components/Landing/molecules/Header/Header.tsx
+++ documentation/src/components/Landing/molecules/Header/Header.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from 'react'
 import Link from '@docusaurus/Link'
 import styles from './styles.module.css'
+const getBasePrefix = () => window.BASE_PREFIX || ''
 
 export const Header: FC = () => (
   <div className={styles.container}>
     <div className={styles.text}>
-      <h1>B-Cloud</h1>
-      <h2>Облако, которое мы заслужили</h2>
-
-      <Link to="/tech-docs/kubernetes/about">
-        <button className={styles.button} type="button">
-          Начать
-        </button>
-      </Link>
+      <h1 className="header-title">
+        <Link to="/tech-docs/kubernetes/about">
+          <img src={`${getBasePrefix()}/img/paws.png`} alt="Logo" className="logo2" />
+        </Link>
+        B-Cloud
+      </h1>
+      <p className="subheading">Строим вместе</p>
     </div>
   </div>
 )
