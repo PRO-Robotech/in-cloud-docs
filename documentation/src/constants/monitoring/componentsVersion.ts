0953652168292be626518fd78a7ada3b3787b4e3
Dmitrii Putilin <dlputilin@dobry-kot.ru>
2025-04-14 13:39:39 +0300

    sync

---
+++ documentation/src/constants/monitoring/componentsVersion.ts
@@ -0,0 +1,25 @@
+import { TComponentsVersionItems } from '../../customTypes/componentsVersion'
+
+export const COMPONENTS_VERSION: TComponentsVersionItems = {
+  victoriaMetricsOperator: {
+    value: '0.37.0',
+  },
+  victoriaMetricsStack: {
+    value: '0.28.2',
+  },
+  nodeExporter: {
+    value: '4.27.0',
+  },
+  blackboxExporter: {
+    value: '9.0.0',
+  },
+  processExporter: {
+    value: '0.5.0',
+  },
+  kubeStateMetrics: {
+    value: '5.16.4',
+  },
+  metricsServer: {
+    value: '3.12.1',
+  },
+}
--- documentation/src/sidebars/technical-docs/monitoring.ts
+++ documentation/src/sidebars/technical-docs/monitoring.ts
@@ -16,15 +16,82 @@ export const monitoring = [
         items: [
           {
             type: 'doc',
-            label: 'empty',
-            id: 'tech-docs/empty',
+            label: '5.5.1.2.1. Node Exporter',
+            id: 'tech-docs/monitoring/exporters/node-exporter/node-exporter-install',
+          },
+          {
+            type: 'doc',
+            label: '5.5.1.2.2. Blackbox Exporter',
+            id: 'tech-docs/monitoring/exporters/blackbox-exporter/blackbox-exporter-install',
+          },
+          {
+            type: 'doc',
+            label: '5.5.1.2.3. Process Exporter',
+            id: 'tech-docs/monitoring/exporters/process-exporter/process-exporter-install',
+          },
+          {
+            type: 'doc',
+            label: '5.5.1.2.4. Kube State Metrics',
+            id: 'tech-docs/monitoring/kube-state-metrics/kube-state-metrics-install',
+          },
+          {
+            type: 'doc',
+            label: '5.5.1.2.5. Metrics Server',
+            id: 'tech-docs/monitoring/metrics-server/metrics-server-install',
+          },
+          {
+            type: 'doc',
+            label: '5.5.1.2.6. VictoriaMetrics Operator',
+            id: 'tech-docs/monitoring/victoria-metrics-operator/victoria-metrics-operator-install',
+          },
+          {
+            type: 'doc',
+            label: '5.5.1.2.7. VictoriaMetrics Cluster',
+            id: 'tech-docs/monitoring/victoria-metrics-cluster/victoria-metrics-cluster-install',
           },
         ],
       },
       {
-        type: 'doc',
-        label: '5.5.1.3. Проверка',
-        id: 'tech-docs/empty',
+        type: 'category',
+        label: '5.5.2.1. Проверка',
+        collapsed: true,
+        items: [
+          {
+            type: 'doc',
+            label: '5.5.2.1.1. Node Exporter',
+            id: 'tech-docs/monitoring/exporters/node-exporter/node-exporter-testing',
+          },
+          {
+            type: 'doc',
+            label: '5.5.2.1.2. Blackbox Exporter',
+            id: 'tech-docs/monitoring/exporters/blackbox-exporter/blackbox-exporter-testing',
+          },
+          {
+            type: 'doc',
+            label: '5.5.2.1.3. Process Exporter',
+            id: 'tech-docs/monitoring/exporters/process-exporter/process-exporter-testing',
+          },
+          {
+            type: 'doc',
+            label: '5.5.2.1.4. Kube State Metrics',
+            id: 'tech-docs/monitoring/kube-state-metrics/kube-state-metrics-testing',
+          },
+          {
+            type: 'doc',
+            label: '5.5.2.1.5. Metrics Server',
+            id: 'tech-docs/monitoring/metrics-server/metrics-server-testing',
+          },
+          {
+            type: 'doc',
+            label: '5.5.2.1.6. VictoriaMetrics Operator',
+            id: 'tech-docs/monitoring/victoria-metrics-operator/victoria-metrics-operator-testing',
+          },
+          {
+            type: 'doc',
+            label: '5.5.2.1.7. VictoriaMetrics Cluster',
+            id: 'tech-docs/monitoring/victoria-metrics-cluster/victoria-metrics-cluster-testing',
+          },
+        ],
       },
     ],
   },
@@ -47,8 +114,38 @@ export const monitoring = [
     items: [
       {
         type: 'doc',
-        label: 'empty',
-        id: 'tech-docs/empty',
+        label: '5.5.3.1. Node Exporter',
+        id: 'tech-docs/monitoring/exporters/node-exporter/node-exporter-setup',
+      },
+      {
+        type: 'doc',
+        label: '5.5.3.2. Blackbox Exporter',
+        id: 'tech-docs/monitoring/exporters/blackbox-exporter/blackbox-exporter-setup',
+      },
+      {
+        type: 'doc',
+        label: '5.5.3.3. Process Exporter',
+        id: 'tech-docs/monitoring/exporters/process-exporter/process-exporter-setup',
+      },
+      {
+        type: 'doc',
+        label: '5.5.3.4. Kube State Metrics',
+        id: 'tech-docs/monitoring/kube-state-metrics/kube-state-metrics-setup',
+      },
+      {
+        type: 'doc',
+        label: '5.5.3.5. Metrics Server',
+        id: 'tech-docs/monitoring/metrics-server/metrics-server-setup',
+      },
+      {
+        type: 'doc',
+        label: '5.5.3.6. VictoriaMetrics Operator',
+        id: 'tech-docs/monitoring/victoria-metrics-operator/victoria-metrics-operator-setup',
+      },
+      {
+        type: 'doc',
+        label: '5.5.3.7. VictoriaMetrics Cluster',
+        id: 'tech-docs/monitoring/victoria-metrics-cluster/victoria-metrics-cluster-setup',
       },
     ],
   },
