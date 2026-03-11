49ff0b637bed9332c288faaf4a911b714b2f5699
pointpu <pointpu@prorobotech.ru>
2026-03-11 13:30:15 +0300

    fix

--- documentation/plugins/medusa.js
+++ documentation/plugins/medusa.js
@@ -1,16 +1,18 @@
 
-export default function () {
+export default function (context) {
+  const baseUrl = context.siteConfig.baseUrl.replace(/\/+$/, '')
   return {
     name: 'medusa',
-    injectHtmlTags({ content }) {
+    injectHtmlTags() {
       return {
         postBodyTags: [
           `<div id="medusa-root"></div>`,
+          `<script>window.BASEURL="${baseUrl}";</script>`,
           {
             tagName: 'script',
             attributes: {
               charset: 'utf-8',
-              src: '/js/header-enabler.js',
+              src: `${context.siteConfig.baseUrl}js/header-enabler.js`,
             },
           },
         ],
